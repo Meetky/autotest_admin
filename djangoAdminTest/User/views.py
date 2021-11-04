ac23b2620d7f97f0518206a29684c9af96ab0693
liukangkang <kangkang.liu@raykite.com>
2021-11-04 16:05:38 +0800

    Login v1

--- djangoAdminTest/User/views.py
+++ djangoAdminTest/User/views.py
@@ -35,8 +35,25 @@ def login(request):
     errorInfo = u""
     detail = {}
     data = request.data
+    print(data)
     username = data.get("username")
     password = data.get("password")
-
+    # 调用django进行用户认证
+    # 验证成功 user返回<class 'django.contrib.auth.models.User'>
+    # 验证失败 user返回None
     user = auth.authenticate(username=username, password=password)
-    print("user: ", user)
+    print("user:", user)
+    if user == None:
+        result = False
+        errorInfo = u"用户名或密码错误"
+        return Response({"result": result, "detail": detail, "errorInfo": errorInfo})
+    # 用户名和密码验证成功
+    # 获取用户的token 如果没有token ，表示时用户首次登录，则进行创建，并且返回token
+    try:
+        tokenObj = Token.objects.get(user_id=user.id)
+    except Exception as e:
+        # token 不存在 说明是首次登录
+        tokenObj = Token.objects.create(user=user)
+    # 获取token字符串
+    token = tokenObj.key
+    return Response({"result": result, "detail": {'token': token}, "errorInfo": errorInfo})
--- djangoAdminTest/djangoAdminTest/settings.py
+++ djangoAdminTest/djangoAdminTest/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +36,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "User",
+    "rest_framework",
+    'rest_framework.authtoken',
 ]
+APPEND_SLASH = False
+
+# # 设置全局身份认证方案
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',  # token认证
+#     )
+# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -70,17 +81,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoAdminTest.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 数据库驱动
+        'NAME': "autotest_admin",  # 数据库名
+        "USER": "root",  # 用户名
+        "PASSWORD": "123456",  # 密码
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -113,7 +129,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
